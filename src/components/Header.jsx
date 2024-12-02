8177dd561c350c6edac3d357146c7f472e0bc606
Max Eidinoff <meidinoff@gmail.com>
2024-12-02 02:19:12 -0500

    Header uses <Link> instead of <a>

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import '../styles/Header.css'
+import { Link } from 'react-router-dom'
 
 const Header = () => (
     <header>
         <div style={{ display: "flex" }}>
-          <h1 id="title" style={{ fontSize: "36px" }}><a href="/Ear-Training-Online/" style={{}}>Ear Training Online</a></h1>
+          <h1 id="title" style={{ fontSize: "36px" }}><Link to="/" style={{ textDecoration: 'none' }}>Ear Training Online</Link></h1>
           <div id="menu-bar" style={{ display: "flex" }}>
-            <a href="/Ear-Training-Online/about">About</a>
+            <Link to="/about">About</Link>
             <a href="https://github.com/meidinoff/Ear-Training-Online" target="_blank" rel="noreferrer noopener">GitHub</a>
           </div>
         </div>
