abdec88ca6b75a7d396684cdecb6a5eb53b25729
Max Eidinoff <meidinoff@gmail.com>
2024-12-02 02:08:01 -0500

    Fix Header links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import '../styles/Header.css'
 const Header = () => (
     <header>
         <div style={{ display: "flex" }}>
-          <h1 id="title" style={{ fontSize: "36px" }}><a href="../../index.html" style={{}}>Ear Training Online</a></h1>
+          <h1 id="title" style={{ fontSize: "36px" }}><a href="/Ear-Training-Online/" style={{}}>Ear Training Online</a></h1>
           <div id="menu-bar" style={{ display: "flex" }}>
-            <a href="/Ear-Training-Online/About">About</a>
+            <a href="/Ear-Training-Online/about">About</a>
             <a href="https://github.com/meidinoff/Ear-Training-Online">GitHub</a>
           </div>
         </div>
