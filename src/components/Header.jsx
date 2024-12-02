30a04de0c36691fe0b57fea5f837ce5892562035
Max Eidinoff <meidinoff@gmail.com>
2024-12-02 02:10:40 -0500

    Open github in new tab

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => (
           <h1 id="title" style={{ fontSize: "36px" }}><a href="/Ear-Training-Online/" style={{}}>Ear Training Online</a></h1>
           <div id="menu-bar" style={{ display: "flex" }}>
             <a href="/Ear-Training-Online/about">About</a>
-            <a href="https://github.com/meidinoff/Ear-Training-Online">GitHub</a>
+            <a href="https://github.com/meidinoff/Ear-Training-Online" target="_blank" rel="noreferrer noopener">GitHub</a>
           </div>
         </div>
     </header>
